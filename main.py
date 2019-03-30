3fd4219dc23cea73e096260310cbf83c1cfd9df7
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-03-30 15:34:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask, redirect, render_template
 
+app = Flask(__name__)
+
+url_for('static', filename = 'index.css')
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/explanation)
+def explanation():
+    return render_template("index2.html")
+if __name__=="__main__":
+    app.run(debug=True)
+           
